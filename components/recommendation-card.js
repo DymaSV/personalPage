7171655cb1c231936c16c385cfdaad276bad5c0d
Dmytro Sv <dymasvyrydonov@gmail.com>
2020-07-16 12:02:04 -0400

    Changed RecommendationCard for index page.

--- components/recommendation-card.js
+++ components/recommendation-card.js
@@ -1,22 +1,60 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
-import Paper from '@material-ui/core/Paper';
 import Avatar from '@material-ui/core/Avatar';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex',
+        justifyContent: "center",
+        '& > *': {
+            margin: theme.spacing(2),
+        },
+    },
+    large: {
+        width: theme.spacing(9),
+        height: theme.spacing(9),
+        margin: 'auto'
+    },
+    text: {
+        textAlign: 'center',
+
+    },
+}));
 export default function RecommendationCard(props) {
+    const [wrap, setWrap] = useState(true);
+    const classes = useStyles();
     return (
         <div>
-            <Paper>
-                <Grid container wrap="nowrap" spacing={2}>
-                    <Grid item>
-                        <Avatar><img src = {props.avatar}/></Avatar>
+            <Card variant="outlined">
+                <CardContent>
+                    <Grid container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        spacing={1}>
+                        <Grid item xs={12} sm={12}>
+                            <Avatar className={classes.large} alt={props.name} src={props.avatar} />
+                        </Grid>
+                        <Grid item xs={12} sm={12} className={classes.text}>
+                            <Typography variant="h7" component="h3">
+                                {props.name}
+                            </Typography>
+                        </Grid>
                     </Grid>
-                    <Grid item xs zeroMinWidth>
-                        <Typography noWrap>{props.message}</Typography>
-                    </Grid>
-                </Grid>
-            </Paper>
+                    {wrap
+                        ? <Typography noWrap variant="body2" component="p">{props.message}</Typography>
+                        : <Typography variant="body2" component="p">{props.message}</Typography>}
+                </CardContent>
+                <CardActions>
+                    <Button size="small" onClick={() => { setWrap(!wrap) }}>See More</Button>
+                </CardActions>
+            </Card>
         </div >
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -17,19 +17,16 @@ export default function Home() {
 
   return (
     <div>
-      <VideoFrame src={'https://www.w3schools.com/html/mov_bbb.mp4'} />
+      <VideoFrame src={'https://vimeo.com/243556536'} />
       <Grid container spacing={5}>
-        <Grid item xs={12} sm={3}>
-          <RecommendationCard avatar='https://media.sproutsocial.com/uploads/2017/08/Social-Media-Video-Specs-Feature-Image.png' message='xvsdsdgsgsdgsdgsdgs'/>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Paper>xvsdsdgsgsdgsdgsdgs</Paper>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Paper>xvsdsdgsgsdgsdgsdgs</Paper>
-        </Grid>
-        <Grid item xs={12} sm={3}>
-          <Paper>xvsdsdgsgsdgsdgsdgs</Paper>
+        <Grid item xs={12} sm={4}>
+          <RecommendationCard 
+          name='Red Cat' 
+          avatar='https://cdn.pixabay.com/photo/2016/09/17/00/53/cat-1675429_960_720.jpg' 
+          message='The cat is a domestic species of small carnivorous mammal.
+           It is the only domesticated species in the family 
+           Felidae and is often referred to as the domestic cat to distinguish 
+           it from the wild members of the family.'/>
         </Grid>
       </Grid>
     </div>
